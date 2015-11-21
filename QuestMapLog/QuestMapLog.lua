c3ff34068243098fe44f0d38f153e1c001a8b3f0
CaptainBlagbird <CaptainBlagbird@mailueberfall.de>
2015-11-21 15:07:58 +0100

    Renamed to Quest Map Log and fixed some minor flaws

---
+++ QuestMapLog/QuestMapLog.lua
@@ -0,0 +1,38 @@
+--[[
+
+Quest Map Log
+by CaptainBlagbird
+https://github.com/CaptainBlagbird
+
+--]]
+
+-- Addon info
+AddonName = "QuestMapLog"
+
+local function GetData()
+	-- Saved variables table
+	QM_Log = {}
+	
+	local id
+	local id
+	-- There currently are < 6000 quests, but some can be completed multiple times.
+	-- 10000 should be more than enough to get all completed quests and still avoid an endless loop.
+	for i=0, 10000 do
+		-- Get next completed quest. If it was the last, break loop
+		id = GetNextCompletedQuestId(id)
+		if id == nil then break end
+		QM_Log[id] = {}
+		QM_Log[id].name, QM_Log[id].questType = GetCompletedQuestInfo(id)
+		QM_Log[id].zoneName, QM_Log[id].objectiveName, QM_Log[id].zoneIndex, QM_Log[id].poiIndex = GetCompletedQuestLocationInfo(id)
+	end
+	-- Reload ui so the saved variables file gets written
+	ReloadUI()
+end
+
+-- Event handler function for EVENT_PLAYER_ACTIVATED
+local function OnPlayerActivated(eventCode)
+	SLASH_COMMANDS["/qmlog"] = GetData
+	
+	EVENT_MANAGER:UnregisterForEvent(AddonName, EVENT_PLAYER_ACTIVATED)
+end
+EVENT_MANAGER:RegisterForEvent(AddonName, EVENT_PLAYER_ACTIVATED, OnPlayerActivated)
---
+++ QuestMapLog/README.md
@@ -0,0 +1,12 @@
+How to
+------
+
+1. In the chat window, enter the command `/qmlog` to collect the data, this also reloads the UI!
+
+2. Find the log file "Elder Scrolls Online\XXX\SavedVariables\QuestMapLog.lua"  
+   (XXX = "live", "liveeu" or "pts")
+
+3. Send me a PM with a link to the file (e.g. upload it to Dropbox, the file probably will be too long for a PM)
+
+____
+[CaptainBlagbird](http://www.esoui.com/forums/member.php?userid=13972)
